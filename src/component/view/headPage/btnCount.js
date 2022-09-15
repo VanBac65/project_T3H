7f4a115666fb0d4b2765b64eca398333df71ad27
VanBac65 <vanbac6500@gmail.com>
2022-09-15 20:24:30 +0700

    fixCategoryList

--- src/component/view/headPage/btnCount.js
+++ src/component/view/headPage/btnCount.js
@@ -3,18 +3,15 @@ import { useDispatch } from 'react-redux';
 import { decrementCountCategoryList, incrementCountCategoryList } from '../../../app/reducer/categoryListSlice';
 
 export default function BtnCount({ elm, index }) {
-    const [del, setDel] = useState(elm.count === 1 ? 'del' : '-')
     const dispatch = useDispatch()
     const handleSub = (elm) => {
-        if (elm.count - 1 !== 1) {
-            setDel('-')
+        console.log(elm)
+        if (elm.count - 1 > 1) {
         }
-        else setDel('del')
         const action = decrementCountCategoryList(index)
         dispatch(action)
     }
     const handleSum = (index) => {
-        setDel('-')
         const action = incrementCountCategoryList(index)
         dispatch(action)
     }
@@ -22,7 +19,7 @@ export default function BtnCount({ elm, index }) {
         <div className='category-item-btn d-flex rounded-pill border'>
             <div className='category-item-btn-action'>
                 <button onClick={() => handleSub(elm)} className='category-item-btn-action1 rounded-pill border-0'>
-                    {del}
+                    {elm.count===1?'del':'-'}
                 </button>
                 <button className='category-item-btn-value rounded-circle btn bg-secondary'>
                     {elm.count}
